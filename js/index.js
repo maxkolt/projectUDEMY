1cd358bfef6190234ec7a048a3599dc3ed94b3b7
Maxim Koltovich <12345kolt@gmail.com>
2020-10-14 14:24:11 +0300

    Проект по курсам udemy.Выполнил первый пункт задания

--- js/index.js
+++ js/index.js
@@ -24,3 +24,12 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
