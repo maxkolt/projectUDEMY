6f00b31144d421baa368dde8dfe3dd58471c6abb
Maxim Koltovich <12345kolt@gmail.com>
2020-10-29 21:47:56 +0300

    Проект по курсам udemy.Выполняю новые пункты  задания

--- js/index.js
+++ js/index.js
@@ -63,5 +63,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 </li>
             `;
         });
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
 
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
+});
 
