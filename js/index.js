e3839c34c7747e5e86ef1c3eeb6894f7cb9ea45d
Maxim Koltovich <12345kolt@gmail.com>
2020-10-29 21:21:12 +0300

    Проект по курсам udemy.Выполняю новые пункты  задания

--- js/index.js
+++ js/index.js
@@ -1,40 +1,5 @@
 'use strict'
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
-
-adv.forEach(item => {
-    item.remove();
-});
-
-genre.textContent = 'Драма';
-
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
 document.addEventListener('DOMContentLoaded', () => {
 
     const adv = document.querySelectorAll('.promo__adv img'),
@@ -75,5 +40,15 @@ document.addEventListener('DOMContentLoaded', () => {
         arr.forEach(item => {
             item.remove();
         });
+    };
+
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
     };
 
